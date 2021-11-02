624adcb6fcb777755e233cd950f26ca880a60a45
Robai <70212311+Robaiii@users.noreply.github.com>
2021-11-02 23:49:04 +0200

    Add files via upload

---
+++ BattleHandHUD.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Jai's BattleHand HUD (Robai repository)
+// @namespace    Robai's repository for Jai's HUD
+// @version      4.8.4
+// @description  BattleHand QoL HUD / Scout interface; Donations may be sent via paypal to randal.t.meyer@gmail.com
+// @author       Randal (aka Jai or JaiLadnar)
+// @updateURL    https://raw.githubusercontent.com/Robaiii/Gaming/main/BattleHandHUD.js
+// @require      https://raw.githubusercontent.com/Robaiii/Gaming/main/default.min.js
+// @require      https://raw.githubusercontent.com/Robaiii/Gaming/main/BattleHandHUD.BattleCards.js
+// @require      https://raw.githubusercontent.com/Robaiii/Gaming/main/BattleHandHUD.Dungeons.js
+// @require      https://raw.githubusercontent.com/Robaiii/Gaming/main/BattleHandHUD.OtherData.js
+// @require      https://raw.githubusercontent.com/Robaiii/Gaming/main/BattleHandHUD.Version.js
+// @include      http://game261051.konggames.com/*
+// @include      http://www.kongregate.com/games/AnotherPlaceProd/*
+// @grant        unsafeWindow
+// ==/UserScript==
+
+bh.hud.listener.init("4.8.4", unsafeWindow);
